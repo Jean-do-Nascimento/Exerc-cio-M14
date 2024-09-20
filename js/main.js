55a5ab38d230c464c99d877cedb9bf6eac3aa4b0
Jean do Nascimento <jeandonascmento@gmail.com>
2024-09-19 21:38:46 -0300

    validation

--- js/main.js
+++ js/main.js
@@ -3,9 +3,30 @@ $(document).ready(function () {
 
     $('form').validate({
 rules: {
+    nome: {
+        required: true
+    },
+    email: {
+        required: true,
+        email: true
+    },
     telefone: {
         required: true,
         tel: true
+    }
+},
+
+messages: {
+    nome: 'este campo é obrigatório',
+    telefone: 'este campo é obrigatório',
+    email: 'este campo é obrigatório',
+    mensagem: 'este campo é obrigatório',
+},
+
+invalidHandler: function (evento, validador) {
+    let camposIncorretos = validador.numberOfInvalids();
+    if (camposIncorretos) {
+        alert(`Existem ${camposIncorretos} campos incorretos`);
     }
 }
 })
